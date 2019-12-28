74561dc6909ed319a0964618cad4d7b6c091c627
FachriAchsan <fr.achsan@gmail.com>
2019-12-28 19:11:22 +0700

    db

---
+++ public/review.php
@@ -0,0 +1,348 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <section id="mu-hero">
+    <?php
+    include('header.php');
+    ?>    
+    <link rel="stylesheet" type="text/css" href="./assets/css/review.css">
+
+</section>
+</head>
+    <body>
+        <!--Kolom Komentar Ke-1-->
+        <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Fachri Achsan | fachriachsan@gmail.com | 12 Sep 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-5.png" width="90px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"Aku baru pertama kali belanja di Mercu Ecommerce. Begitu sampai, aku puas sama bukunya, rapi, ori.
+             Nggak mengecewakan, diskonnya juga mantap. Kapan-kapan aku mau belanja lagi. Semoga aku betah belanja disini."</p>
+             <img src="./assets/images/book-1.jfif" width="90px" height="90px">
+             <h5>-Cikampek-</h5>
+        </div>
+        <br>
+        <!--Kolom Komentar Ke-2-->
+        <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Muhammad Fauqi Ikhsan | ikhsanfauqi@gmail.com | 14 Sep 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-4.png" width="90px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"saya tidak pernah beli buku secara online, namun kami sangat senang dengan tampilan menu di web ini, 
+            bahkan di web ini selalu tampil buku-buku terbaru, inilah yang membuat tertarik terhadap keinginan kami untuk belanja di ini."</p>
+            <img src="./assets/images/book-2.jfif" width="90px" height="90px">
+            <h5>-Tanjung Priok, Jakarta Utara-</h5>
+        </div>
+        <br>
+        <!--Kolom Komentar Ke-3-->
+        <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Ardian Maulana | ardian.maulana@gmail.com | 17 Sep 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-4.png" width="90px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>Terima kasih mercu ecommerce sdh 3 tahun saya tidak belanja. baru sekarang belanja lagi ternyata pelayanannya semakin bagus banget. 
+                paket barang saya terima lebih cepat. terimakasih bukukita. luv you bukukita.</p>
+                <img src="./assets/images/book-3.jpg" width="90px" height="90px">
+            <h5>-Bogor, Jawa Barat-</h5>
+        </div>
+        <br>
+        <!--Kolom Komentar Ke-4-->
+        <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Heri Kurniawan | kurniawan.her@gmail.com | 19 Sep 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-5.png" width="90px" height="20px" ></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"Ini kali kedua aku beli buku di mercu ecommerce. Suka banget dengan bukunya, ada hadiah setiap bulannya, pelayanannya bagus, pengirimannya cepat.
+                 Puas sekali beli buku disinii. Terus berkarya bukukita. Terima kasih"</p>
+                 <img src="./assets/images/book-4.jpg" width="90px" height="90px">
+             <h5>-Balikpapan, Kalimantan Timur-</h5>
+        </div>
+         <br>
+         <!--Kolom Komentar Ke-5-->
+        <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Rendy T Simbar | rendisimbar@yahoo.com | 23 Sep 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-5.png" width="90px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"Berkali-kali beli buku di Mercu Ecommerce pelayanan memuaskan 1 hari buku telah ada langsung di kirim ke rumah. Semua buku yg saya inginkan ada semua di Bukukita.
+                 Semakin bersemangat membaca buku ya nih. Semoga Bukukita menjadi yg terdepan u/ pembelian buku online di Indonesia."</p>
+                 <img src="./assets/images/book-5.jfif" width="90px" height="90px">
+            <h5>-Surabaya, Jawa Timur-</h5>
+        </div>
+        <br>
+        <!--Kolom Komentar Ke-6-->
+        <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Ardan Hidayat | ardan.hidayat@gmail.com | 27 Sep 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-3.png" width="90px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"Berapa kali beli di Mercu Ecommerce selalu sesuai ekspektasi.
+                 Ori dan gak perlu susah lagi jauh2 mencari toko buku yang pas dikantong dan pas dihati.."</p>
+                 <img src="./assets/images/book-6.jpg" width="90px" height="90px">
+             <h5>-Semarang, Jawa Tengah-</h5>
+        </div>
+        <br>
+        <!--Kolom Komentar Ke-7-->
+        <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Muhammad Rizky Subagia |subagia.rizky@gmail.com | 29 Sep 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-4.png" width="90px" height="20px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"pertama kali beli buku di Mercu Ecommerce, pelayanannya sangat bagus, ramah, buku-bukunya banyak diskon, pengirimannya cepat.
+                 recomended dech!"</p>
+                 <img src="./assets/images/book-7.jpg" width="90px" height="90px">
+            <h5>-Bandung, Jawa Barat-</h5>
+        </div>
+        <br>
+        <!--Kolom Komentar Ke-8-->
+        <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Adnan Maulana | adnanmaulana@yahho.com | 1 Okt 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-5.png" width="90px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"Dua kali belanja disini dan hasilnya memuaskan, produk ori dan senangnya ada layanan pesan ke penerbit.
+                 Jadinya gak mesti bolak balik web untuk lihat ready tidaknya. Terimakasih Mercu Ecommerce."</p>
+                 <img src="./assets/images/book-8.jpg" width="90px" height="90px">
+            <h5>-Tasikmalaya, Jawa Barat-</h5>
+        </div>
+        <br>
+         <!--Kolom Komentar Ke-9-->
+         <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Farhat Abas | abasfarhas112@gmail.com 3 Okt 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-4.png" width="90px" height="20px" ></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"WOW. diskon nya itu loo. Kadang lebih milih di Mercu Ecommerce, soalnya kita bisa dapet banyak POIN, HADIAH, dengan cara yang mudah.
+                 Mercu Ecommerce cocok bgt deh buat orang yang suka beli dan baca buku, gak pernah khawatir ketinggalan info BUKU TERUPDATE."</p>
+                 <img src="./assets/images/book-9.jfif" width="90px" height="90px">
+            <h5>-Bandung, Jawa Barat-</h5>
+        </div>
+         <!--Kolom Komentar Ke-10-->
+         <br>
+         <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Anggi Maulana | giemaulana@gmail.com | 11 Okt 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-5.png" width="90px" height="20px" ></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"Alhamdulillaah... Sudah 3x order di bukukita.com dan ini order keempat untuk kirim buku ke seorang sahabat saya. Tidak pernah kecewa karena pelayanannya baik. Pernah ditelepon ketika bukunya tidak ada dan amanah dengan mengembalikan kelebihan uang.
+                 Responnya cepat. Semoga langgeng terus Mercu Ecommerce. :)."</p>
+                 <img src="./assets/images/book-10.jfif" width="90px" height="90px">
+            <h5>-Bandar Lampung-</h5>
+        </div>
+          <!--Kolom Komentar Ke-11-->
+          <br>
+          <div id="Form-Komentar-2">
+             <table>
+                 <tr>
+                     <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                     <td colspan="2">Muhammad Aziz | M.aziz@gmail.com | 13 Okt 2019</td>
+                 </tr>
+                 <tr>
+                     <td><img src="./assets/images/star-5.png" width="90px" height="20px"></td>
+                 </tr>
+             </table>
+             <hr>
+             <p>"Pertama beli di Mercu Ecommerce, tapi seneng bgt sama pelayanannya cepet, banyak diskon, bukunya ori lagi"</p>
+             <img src="./assets/images/book-11.jpg" width="90px" height="90px">
+             <h5>-Karawang, Jawa Barat-</h5>
+         </div>
+         <!--Kolom Komentar Ke-12-->
+         <br>
+         <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Bayu Nugraha | nugrahabayu12@gmail.com | 14 Okt 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-4.png" width="90px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"Belanja di Mercu Ecommerce bikin ketagihan, selain harga yang selalu discount, komunikasinya cepat apabila barang tidak tersedia.
+             Buku original, ada hadiah di setiap bulannya. selalu pingin pesan lagi dan lagi........"</p>
+             <img src="./assets/images/book-12.jfif" width="90px" height="90px">
+            <h5>-Bandung, Jawa Barat-</h5>
+        </div>
+         <!--Kolom Komentar Ke-13-->
+         <br>
+         <div id="Form-Komentar-2">
+            <table>
+                <tr>
+                    <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                    <td colspan="2">Taufik Hidayat | taufikhidayat@gmail.com | 16 Okt 2019</td>
+                </tr>
+                <tr>
+                    <td><img src="./assets/images/star-4.png" width="90px" height="20px"></td>
+                </tr>
+            </table>
+            <hr>
+            <p>"Kenapa ga dari dlu tau Mercu Ecommerce, Ternyata emang lebih murah dari toko sebelah, adminnya fast respon lagi, mantul dech bukunya"</p>
+            <img src="./assets/images/book-13.jpg" width="90px" height="90px">
+            <h5>-Kemayoran, Jakarta Pusat-</h5>
+        </div>
+          <!--Kolom Komentar Ke-14-->
+          <br>
+          <div id="Form-Komentar-2">
+             <table>
+                 <tr>
+                     <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                     <td colspan="2">Regian Maulana | egiw.regian@gmail.com | 18 Okt 2019</td>
+                 </tr>
+                 <tr>
+                     <td><img src="./assets/images/star-5.png" width="90px" height="20px"></td>
+                 </tr>
+             </table>
+             <hr>
+             <p>"Kenapa ga dari dlu tau Mercu Ecommerce, Ternyata emang lebih murah dari toko sebelah, adminnya fast respon lagi, mantul dech bukunya"</p>
+             <img src="./assets/images/book-14.jpeg" width="90px" height="90px">
+             <h5>-Palembang, Sumatera Selatan-</h5>
+         </div>
+          <!--Kolom Komentar Ke-15-->
+          <br>
+          <div id="Form-Komentar-2">
+             <table>
+                 <tr>
+                     <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                     <td colspan="2">Raffi ahmad | ahmadraffi@gmail.com | 21 Okt 2019</td>
+                 </tr>
+                 <tr>
+                     <td><img src="./assets/images/star-4.png" width="90px" height="20px"></td>
+                 </tr>
+             </table>
+             <hr>
+             <p>"Kali kedua beli buku di Mercu Ecommerce ... Packing nya rapi , pengiriman cepat , admin nya juga ramah :) selalu puas beli buku disini .. Luv"</p>
+             <img src="./assets/images/book-15.jfif" width="90px" height="90px">
+             <h5>-Bandung, Jawa Barat-</h5>
+         </div>
+          <!--Kolom Komentar Ke-16-->
+          <br>
+          <div id="Form-Komentar-2">
+             <table>
+                 <tr>
+                     <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                     <td colspan="2">Jainudin | jaenudinn@gmail.com | 22 Okt 2019</td>
+                 </tr>
+                 <tr>
+                     <td><img src="./assets/images/star-4.png" width="90px" height="20px"></td>
+                 </tr>
+             </table>
+             <hr>
+             <p>"Puas belanja disini, sudah 4x memuaskan terima kasih Mercu Ecommerce"</p>
+             <img src="./assets/images/book-16.jfif" width="90px" height="90px">
+             <h5>-Cilamaya, Jawa Barat-</h5>
+         </div>
+           <!--Kolom Komentar Ke-17-->
+           <br>
+           <div id="Form-Komentar-2">
+              <table>
+                  <tr>
+                      <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                      <td colspan="2">Baharudin | kangbahar@gmail.com | 23 Okt 2019</td>
+                  </tr>
+                  <tr>
+                      <td><img src="./assets/images/star-4.png" width="90px" height="20px"></td>
+                  </tr>
+              </table>
+              <hr>
+              <p>"Ini sudah ke 2 kalinya saya belanja buku disini dan memuaskan, dari pelayanannya sampai pengirimannya bagus"</p>
+              <img src="./assets/images/book-17.jpg" width="90px" height="90px">
+              <h5>-Kemayoran, Jakarta Pusat-</h5>
+          </div>
+            <!--Kolom Komentar Ke-18-->
+            <br>
+            <div id="Form-Komentar-2">
+               <table>
+                   <tr>
+                       <td rowspan="2"><img src="./assets/images/icon.png" width="40px" height="40px"></td>
+                       <td colspan="2">Jamal Mustofa | jamalmusr@gmail.com | 24 Okt 2019</td>
+                   </tr>
+                   <tr>
+                       <td><img src="./assets/images/star-5.png" width="90px" height="20px"></td>
+                   </tr>
+               </table>
+               <hr>
+               <p>"Saya puas belanja di sini stock buku lengkap dan banyak dan yg terpenting murah harganya .... "</p>
+               <img src="./assets/images/book-14.jpeg" width="90px" height="90px">
+               <h5>-Semarang, Jawa Tengah-</h5>
+           </div>
+         <br>
+         <form id="formKomentar">
+            <div>
+             <input type="text" name="nama" placeholder="Nama Lengkap" />
+            </div>
+      
+            <div>
+             <input type="text" name="email" placeholder="Alamat Email" />
+          </div>
+      
+            <div>
+             <textarea name="komentar" rows="10" placeholder="Komentar Anda"></textarea>
+            </div>
+      
+            <div>
+              <input type="submit" value="Tambahkan Komentar"/>
+            </div>
+        </form>
+    </body>
+</html>
